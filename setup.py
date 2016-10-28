bcd0fcef250a8b6d315208179c1e462968fbb708
kjaym <kylemichel@gmail.com>
2016-10-28 11:05:49 -0700

    update citrination-client to 1.1.16

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='citrination',
-      version='1.0.6',
+      version='1.0.7',
       url='http://github.com/CitrineInformatics/python-citrination-cli',
       description='Command line interface for interacting with Citrination sites',
       packages=find_packages(),
       install_requires=[
             'argparse',
             'pyCLI==2.0.3',
-            'citrination-client==1.1.15'
+            'citrination-client==1.1.16'
       ],
       entry_points={
             'console_scripts': [
